216cda5d8d1a8d0af1ac0e5f85264a385ebfb965
H0NG <yunjin5004@naver.com>
2025-03-30 21:53:53 +0900

    * 25. 03. 30
    create project

---
+++ src/main/java/hello/exception/KyhMvc2ExceptionApplication.java
@@ -0,0 +1,13 @@
+package hello.exception;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KyhMvc2ExceptionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KyhMvc2ExceptionApplication.class, args);
+    }
+
+}
